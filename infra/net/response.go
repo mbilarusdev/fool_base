b4a76cc26efe52f61fcbcde1ba1abfaf76655077
mbilarusdev <maxim.bilarus@gmail.com>
2025-10-02 17:16:57 +0300

    added response utils

---
+++ infra/net/response.go
@@ -0,0 +1,29 @@
+package net
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Code  int    `json:"code"`
+	Data  any    `json:"data,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+func WriteResponse(w http.ResponseWriter, code int, data any) {
+	response := Response{
+		Code: code,
+		Data: data,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(response)
+}
+
+func WriteError(w http.ResponseWriter, code int, errMsg string) {
+	WriteResponse(w, code, Response{
+		Error: errMsg,
+	})
+}
