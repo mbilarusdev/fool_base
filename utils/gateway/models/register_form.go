456b703d2f0a04e03f7d39f8fbfe3f709c793d2b
mbilarusdev <maxim.bilarus@gmail.com>
2025-10-02 23:29:52 +0300

    fixed endpoints to list

--- utils/gateway/models/register_form.go
+++ utils/gateway/models/register_form.go
@@ -1,9 +1,9 @@
 package gateway_models
 
 type RegisterForm struct {
-	ServiceName       string    `json:"service_name"`
-	InstanceID        string    `json:"instance_id"`
-	Endpoints         *Endpoint `json:"resolvers"`
+	ServiceName       string     `json:"service_name"`
+	InstanceID        string     `json:"instance_id"`
+	Endpoints         []Endpoint `json:"resolvers"`
 	GatewayAddr       string
 	GatewayProtocol   string
 	ServiceRemoteAddr string `json:"service_remote_addr"`
